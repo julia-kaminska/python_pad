0f498499345f08ea2cafa42c16a0524c97ff4575
julia-kaminska <kaminska.julia@interia.pl>
2023-10-11 21:57:59 +0200

    Minor change in task 2

--- homework/task_2.py
+++ homework/task_2.py
@@ -1,6 +1,6 @@
-def unique_list(my_list):
+def unique_list(input_list):
     final_list = []
-    for item in my_list:
+    for item in input_list:
         if item not in final_list:
             final_list.append(item)
     return final_list
